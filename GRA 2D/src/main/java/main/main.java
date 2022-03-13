c035d378a32ee7760233f0a45128f4a7a727d7a4
Michał Motyka <michmot12@gmail.com>
2022-03-13 12:18:14 +0100

    inicial structures of project in java

---
+++ GRA 2D/src/main/java/main/main.java
@@ -0,0 +1,19 @@
+package main;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class main {
+    public static void main(String args[]) throws IOException {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("GRA");
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        gamePanel.startGameThread();
+    }
+}
