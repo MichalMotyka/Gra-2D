9a72bf14070177b523e359485fe5f485cda3563c
Gabriel Pazdan <gabriel.pazdan1@gmail.com>
2022-03-26 15:04:10 +0100

    Database data protection

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,10 +2,17 @@
 exports.__esModule = true;
 exports.pool = void 0;
 var promise_1 = require("mysql2/promise");
+var db_key_1 = require("./db-key");
 exports.pool = (0, promise_1.createPool)({
-    host: 'localhost',
-    user: 'root',
-    database: '2d_game',
+    // host: 'localhost',
+    // user: 'root',
+    // database: '2d_game',
+    // decimalNumbers: true,
+    // namedPlaceholders: true,
+    host: db_key_1.DB_HOST,
+    user: db_key_1.DB_USER,
+    database: db_key_1.DB_DATABASE,
+    password: db_key_1.DB_PASSWORD,
     decimalNumbers: true,
     namedPlaceholders: true
 });
--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,15 +1,16 @@
 import {createPool} from "mysql2/promise";
+import {DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER} from "./db-key";
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    database: '2d_game',
-    decimalNumbers: true,
-    namedPlaceholders: true,
-    // host: 'serwer2216476.home.pl',
-    // user: '35854268_2dgame',
-    // database: '35854268_2dgame',
-    // password: 'Trudnehaslo12@',
+    // host: 'localhost',
+    // user: 'root',
+    // database: '2d_game',
     // decimalNumbers: true,
     // namedPlaceholders: true,
+    host: DB_HOST,
+    user: DB_USER,
+    database: DB_DATABASE,
+    password: DB_PASSWORD,
+    decimalNumbers: true,
+    namedPlaceholders: true,
 })
